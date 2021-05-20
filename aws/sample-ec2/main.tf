5350350ba4f34f5bd07ded4ae4c07bff5ec33434
Francisco Ribeiro <francisco.junior@harness.io>
2021-05-20 12:28:11 -0300

    Add Key_name var

--- aws/sample-ec2/main.tf
+++ aws/sample-ec2/main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "sample_ec2" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
+  key_name = "${var.key_name}"
   tags = {
     "Name" = "Francisco - Terraform",
     "ttl" = "2",
--- aws/sample-ec2/tfVars.tf
+++ aws/sample-ec2/tfVars.tf
@@ -4,3 +4,5 @@ variable "region" {
 
 variable "access_key" {}
 variable "secret_key" {}
+
+variable "key_name" {}
