7692c3f198cb629c0f3b996ca4660ca6b7720f62
Francisco Ribeiro <francisco.junior@harness.io>
2021-05-20 12:01:33 -0300

    Add comma

--- aws/sample-ec2/main.tf
+++ aws/sample-ec2/main.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "sample_ec2" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
   tags = {
-    "Name" = "Francisco - Terraform"
-    "ttl" = "2"
+    "Name" = "Francisco - Terraform",
+    "ttl" = "2",
     "usage" = "worker"
   }
 }
