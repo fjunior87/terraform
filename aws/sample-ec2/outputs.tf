4614671c98a719fccf7ab7ccea6d8deed001d2b3
Francisco Ribeiro <francisco.junior@harness.io>
2021-05-21 10:53:53 -0300

    Fix values retrieval

--- aws/sample-ec2/outputs.tf
+++ aws/sample-ec2/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.sample_ec2.*.id
+  value       = aws_instance.sample_ec2.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.sample_ec2.*.public_ip
+  value       = aws_instance.sample_ec2.public_ip
 }
 
 output "instance_tags" {
   description = "Tags of the EC2 instance"
-  value       = element(aws_instance.sample_ec2.*.tags, 0)
+  value       = aws_instance.sample_ec2.tags
 }
