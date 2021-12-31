4569985cbde0b3b675ff7d83eb6c0f58bb30b2c8
Francisco Ribeiro Junior <fjunior87@gmail.com>
2021-12-31 14:48:32 -0300

    Create main.tf

---
+++ sample/main.tf
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# PIN TERRAFORM VERSION TO 0.11.15
+# ---------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = "= 0.11.15"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# BASIC TERRAFORM EXAMPLE
+# See test/terraform_aws_example.go for how to write automated tests for this code.
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "template_file" "example" {
+  template = "example"
+}
+
+data "template_file" "example2" {
+  template = "example1"
+}
+
+resource "local_file" "example" {
+  content  = "${data.template_file.example.rendered} + ${data.template_file.example2.rendered}"
+  filename = "example.txt"
+}
+
+resource "local_file" "example2" {
+  content  = "${data.template_file.example2.rendered}"
+  filename = "example2.txt"
+}
