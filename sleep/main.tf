39d87a79915c90e318085211a7422fe4ba807bd1
Francisco Junior <francisco.junior@harness.io>
2023-01-05 14:38:44 -0300

    sleep 12 minutes

--- sleep/main.tf
+++ sleep/main.tf
@@ -13,13 +13,13 @@ terraform {
 
 resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_30_seconds" {
+resource "time_sleep" "wait_12_minutes" {
   depends_on = [null_resource.previous]
 
-  create_duration = "30s"
+  create_duration = "12m"
 }
 
 # This resource will create (at least) 30 seconds after null_resource.previous
 resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_30_seconds]
+  depends_on = [time_sleep.wait_12_minutes]
 }
