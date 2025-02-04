46d0d9607263a28e2a98a3af414627f290418e50
Francisco Ribeiro Junior <fjunior87@gmail.com>
2025-02-04 15:48:04 -0300

    Update main.tf

--- sample/main.tf
+++ sample/main.tf
@@ -11,20 +11,20 @@ terraform {
 # See test/terraform_aws_example.go for how to write automated tests for this code.
 # ---------------------------------------------------------------------------------------------------------------------
 
-data "template_file" "example" {
-  template = "example"
-}
+#data "template_file" "example" {
+#  template = "example"
+#}
 
-data "template_file" "example2" {
-  template = "example1"
-}
+#data "template_file" "example2" {
+#  template = "example1"
+#}
 
 resource "local_file" "example" {
-  content  = "${data.template_file.example.rendered} + ${data.template_file.example2.rendered}"
+  content  = "abc def"
   filename = "example.txt"
 }
 
 resource "local_file" "example2" {
-  content  = "${data.template_file.example2.rendered}"
+  content  = "ghi jlm"
   filename = "example2.txt"
 }
